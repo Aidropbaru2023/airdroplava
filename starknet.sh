6ffcd43bfc8c6fb74d2e6d77a4bce7003587c408
Aidropbaru2023 <136803003+Aidropbaru2023@users.noreply.github.com>
2024-06-10 22:32:08 -0700

    starknet.sh

--- starknet.sh
+++ starknet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://rpc.starknet.lava.build/lava-referer-52c07349-8833-4d36-a39b-6e23d93180c5/"
 
 OUTPUT_DIR="logs"
 
